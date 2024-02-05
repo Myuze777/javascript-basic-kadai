ae0f594c891ac767a25e34ab52e75b0ee5c5837c
Myuze <146706486+Myuze777@users.noreply.github.com>
2024-02-05 18:08:50 +0900

    課題を提出致します
    確認をよろしくお願いいたします。

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,4 +1,4 @@
-num = 80
+num = 28
 console.log(num);
 
 if (num %3 === 0 && num %5 === 0) {
@@ -7,4 +7,6 @@ if (num %3 === 0 && num %5 === 0) {
     console.log('5の倍数です');
 }else if (num %3 === 0) {
     console.log('3の倍数です');
+}else {
+    console.log(num);
 }
